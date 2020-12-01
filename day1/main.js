67f3a3ba440f4aea638971975ececefe567c93fd
Luis Rovirosa Mairlot <luis@codium.team>
2020-12-01 20:56:29 +0100

    [day 1] improve the log

--- day1/main.js
+++ day1/main.js
@@ -4,11 +4,11 @@ const file = fs.readFileSync('input.txt', 'utf8');
 const numbers = file.split("\n")
 
 const combinations = numbers.flatMap(
-    (v, i) => numbers.slice(i+1).map( w => [parseInt(v),parseInt(w)] )
+    (v, i) => numbers.slice(i+1).map( w => [parseInt(v), parseInt(w)] )
 );
 
 const [number1, number2] = combinations.find(([a, b]) => (a + b) === 2020)
-console.log(number1, number2);
+console.log({number1, number2});
 
 const result = number1 * number2
 console.log(result);
