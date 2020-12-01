494f6897ce6aa6fee1887248636f310d00f8c56d
Luis Rovirosa Mairlot <luis@codium.team>
2020-12-01 21:24:12 +0100

    refactor change loop into flatMap with 3 levels

--- day1/main.js
+++ day1/main.js
@@ -5,7 +5,7 @@ const numbers = file.split('\n').slice(0, -1); // Phpstorm adds an empty line at
 
 function find2020InGroupsOf2() {
     const combinationsOf2Elements = numbers.flatMap(
-        (v, i) => numbers.slice(i + 1).map(w => [parseInt(v), parseInt(w)]),
+        (x, i) => numbers.slice(i + 1).map(y => [parseInt(x), parseInt(y)]),
     );
 
     const [number1, number2] = combinationsOf2Elements.find(([a, b]) => (a + b) === 2020);
@@ -18,14 +18,11 @@ function find2020InGroupsOf2() {
 find2020InGroupsOf2();
 
 function find2020InGroupsOf3() {
-    let combinationsOf3Elements = [];
-    for (i = 0; i < numbers.length - 2; i++) {
-        for (j = i + 1; j < numbers.length - 1; j++) {
-            for (k = j + 1; k < numbers.length; k++) {
-                combinationsOf3Elements.push([parseInt(numbers[i]), parseInt(numbers[j]), parseInt(numbers[k])]);
-            }
-        }
-    }
+    const combinationsOf3Elements = numbers.flatMap(
+        (x, i) => numbers.slice(i + 1).flatMap(
+            (y, j) => numbers.slice(i + j + 1).map(w => [parseInt(x), parseInt(y), parseInt(w)]),
+        ),
+    );
 
     const [number1, number2, number3] = combinationsOf3Elements.find(([a, b, c]) => (a + b + c) === 2020
     );
