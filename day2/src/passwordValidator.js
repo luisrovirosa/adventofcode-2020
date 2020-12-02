c1c4ea31d64ff6ae63720096d8178b1ea6d4aaf7
Luis Rovirosa Mairlot <luis@codium.team>
2020-12-02 20:41:58 +0100

    [day 2] refactor extract isValid method

--- day2/src/passwordValidator.js
+++ day2/src/passwordValidator.js
@@ -1,9 +1,14 @@
-function countValidPasswords(policyAndPassword) {
+function isValid(policyAndPassword) {
     const [policy, password] = policyAndPassword.split(': ');
     const [times, requiredLetter] = policy.split(' ');
     const [minimumNumberOfTimes, maximumNumberOfTimes] = times.split('-');
     let numberOfTimes = password.split(requiredLetter).length - 1;
-    return numberOfTimes >= minimumNumberOfTimes && numberOfTimes <= maximumNumberOfTimes ? 1: 0;
+
+    return numberOfTimes >= minimumNumberOfTimes && numberOfTimes <= maximumNumberOfTimes;
+}
+
+function countValidPasswords(policyAndPassword) {
+    return isValid(policyAndPassword) ? 1 : 0;
 }
 
 export {countValidPasswords};
