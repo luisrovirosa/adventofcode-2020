d13cbfd377dc66d2e0538f0db56cbc5b11f76338
Luis Rovirosa Mairlot <luis@codium.team>
2020-12-02 20:09:45 +0100

    [day 2] add describe block

--- day2/src/passwordValidator.spec.js
+++ day2/src/passwordValidator.spec.js
@@ -1,5 +1,7 @@
 import {countValidPasswords} from './passwordValidator';
 
-test('password only with required letters is valid', () => {
-    expect(countValidPasswords('1-2 m: m')).toBe(1);
+describe('valid passwords', () => {
+    test('password only with required letters is valid', () => {
+        expect(countValidPasswords('1-2 m: m')).toBe(1);
+    });
 });
