f9d6d598283b08c70dba6b18622b150d28d6653a
Luis Rovirosa Mairlot <luis@codium.team>
2020-12-03 19:03:17 +0100

    [day 3] refactor

--- day3/src/trajectory.js
+++ day3/src/trajectory.js
@@ -3,8 +3,8 @@ function numberOfTrees(movement, mapAsString) {
   let numberOfTrees = 0;
   let x = 0;
   let y = 0;
-  while (y < map.length) {
-    if (!fitsInTheMap(x, map)){
+  while (hasNotArrived(y, map)) {
+    if (doesNotFitInTheMap(x, map)) {
       map = extendTheMap(map);
     }
     if (hasATree(x, y, map)) {
@@ -16,8 +16,12 @@ function numberOfTrees(movement, mapAsString) {
   return numberOfTrees;
 }
 
-function fitsInTheMap(x, map) {
-  return x <= map[0].length;
+function hasNotArrived(y, map) {
+  return y < map.length;
+}
+
+function doesNotFitInTheMap(x, map) {
+  return x > map[0].length;
 }
 
 function extendTheMap(map) {
@@ -28,7 +32,5 @@ function hasATree(x, y, map) {
   let square = map[y][x];
   return square === '#';
 }
-
-
 
 export {numberOfTrees};
