ce1bb16388597ca7956d2cb43302ecf07cc52ffd
Luis Rovirosa Mairlot <luis@codium.team>
2020-12-03 17:56:53 +0100

    [day 3] preparatory refactoring to have position multi dimension

--- day3/src/trajectory.js
+++ day3/src/trajectory.js
@@ -1,11 +1,11 @@
 function hasATree(position, map) {
-  let square = map[0][position];
+  let square = map[position[0]][position[1]];
   return square === '#';
 }
 
 function numberOfTrees(movement, mapAsString) {
   const map = mapAsString.split('\n');
-  let position = 0;
+  let position = [0, 0];
   let numberOfTrees = 0;
   if (hasATree(position, map)) {
     numberOfTrees++;
