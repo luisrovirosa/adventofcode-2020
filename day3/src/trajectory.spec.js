eb51a927a7df2e0db1ec707c3aa206254b722f2a
Luis Rovirosa Mairlot <luis@codium.team>
2020-12-03 17:40:25 +0100

    [day 3] ignore

--- day3/src/trajectory.spec.js
+++ day3/src/trajectory.spec.js
@@ -1,12 +1,11 @@
 import {numberOfTrees} from './numberOfTrees';
 
 describe('No trajectory', () => {
-    test('find no tree when the initial position has no tree', () => {
-        expect(numberOfTrees([], '.#..........#......#..#.....#..')).toBe(0);
-    });
+  test('find no tree when the initial position has no tree', () => {
+    expect(numberOfTrees([], '.#..........#......#..#.....#..')).toBe(0);
+  });
 
-    test('find one tree when the initial position has a tree', () => {
-        expect(numberOfTrees([], '#...........#......#..#.....#..')).toBe(1);
-    });
-
+  test('find one tree when the initial position has a tree', () => {
+    expect(numberOfTrees([], '#...........#......#..#.....#..')).toBe(1);
+  });
 });
