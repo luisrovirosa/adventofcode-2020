6c9e3c376c4736b388331da3ab5328f884ed8731
Luis Rovirosa Mairlot <luis@codium.team>
2020-12-03 18:28:36 +0100

    [day 3] change index for better comprehension

--- day3/src/trajectory.js
+++ day3/src/trajectory.js
@@ -1,5 +1,5 @@
 function hasATree(position, map) {
-  let square = map[position[0]][position[1]];
+  let square = map[position[1]][position[0]];
   return square === '#';
 }
 
@@ -7,7 +7,7 @@ function numberOfTrees(movement, mapAsString) {
   const map = mapAsString.split('\n');
   let numberOfTrees = 0;
   for (let i = 0; movement[1] * i < map.length; i++) {
-    if (hasATree([movement[1]*i, movement[0]*i], map)) {
+    if (hasATree([movement[0]*i, movement[1]*i], map)) {
       numberOfTrees++;
     }
   }
