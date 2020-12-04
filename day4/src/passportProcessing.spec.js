f1370f6133ce4d124e90c66ba277df35309bfb10
Luis Rovirosa Mairlot <luis@codium.team>
2020-12-04 18:15:25 +0100

    [day 4] skipped multiple passports sample acceptance test

--- day4/src/passportProcessing.spec.js
+++ day4/src/passportProcessing.spec.js
@@ -57,3 +57,24 @@ describe('invalid passports', () => {
         expect(countValidPassports(passport)).toBe(0);
     });
 });
+
+
+describe('multiple passports', () => {
+   xtest('sample acceptance test', () => {
+       let passport = 'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\n' +
+           'byr:1937 iyr:2017 cid:147 hgt:183cm\n' +
+           '\n' +
+           'iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\n' +
+           'hcl:#cfa07d byr:1929\n' +
+           '\n' +
+           'hcl:#ae17e1 iyr:2013\n' +
+           'eyr:2024\n' +
+           'ecl:brn pid:760753108 byr:1931\n' +
+           'hgt:179cm\n' +
+           '\n' +
+           'hcl:#cfa07d eyr:2025 pid:166559648\n' +
+           'iyr:2011 ecl:brn hgt:59in';
+
+       expect(countValidPassports(passport)).toBe(2);
+   })
+});
