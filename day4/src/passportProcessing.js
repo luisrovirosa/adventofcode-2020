010312536f6ff6a7640f426c7e3ade426c8d40db
Luis Rovirosa Mairlot <luis@codium.team>
2020-12-04 18:11:18 +0100

    [day 4] invalid missing Issue Year (iyr)

--- day4/src/passportProcessing.js
+++ day4/src/passportProcessing.js
@@ -1,6 +1,6 @@
 function countValidPassports(passports) {
   let isValid = passports.includes('ecl') && passports.includes('pid') && passports.includes('eyr') &&
-      passports.includes('hcl') && passports.includes('byr') ;
+      passports.includes('hcl') && passports.includes('byr') && passports.includes('iyr') ;
   return isValid ? 1 : 0;
 }
 
