2692c9be97d22563f444e6affe100b687f11216f
Luis Rovirosa Mairlot <luis@codium.team>
2020-12-04 18:24:52 +0100

    [day 4] refactor extract explaining methods

--- day4/src/passportProcessing.js
+++ day4/src/passportProcessing.js
@@ -1,16 +1,44 @@
-function isValidPassport(passport) {
-  return passport.includes('ecl') &&
-      passport.includes('pid') &&
-      passport.includes('eyr') &&
-      passport.includes('hcl') &&
-      passport.includes('byr') &&
-      passport.includes('iyr') &&
-      passport.includes('hgt');
-}
-
 function countValidPassports(passportsAsString) {
   let passports = passportsAsString.split('\n\n');
   return passports.filter(isValidPassport).length;
+}
+
+function isValidPassport(passport) {
+  return includesEyeColor(passport) &&
+      includesPassportId(passport) &&
+      includesExpirationYear(passport) &&
+      includesHairColor(passport) &&
+      includesBirthYear(passport) &&
+      includesIssueYear(passport) &&
+      includesHeight(passport);
+}
+
+function includesEyeColor(passport) {
+  return passport.includes('ecl');
+}
+
+function includesPassportId(passport) {
+  return passport.includes('pid');
+}
+
+function includesExpirationYear(passport) {
+  return passport.includes('eyr');
+}
+
+function includesHairColor(passport) {
+  return passport.includes('hcl');
+}
+
+function includesBirthYear(passport) {
+  return passport.includes('byr');
+}
+
+function includesIssueYear(passport) {
+  return passport.includes('iyr');
+}
+
+function includesHeight(passport) {
+  return passport.includes('hgt');
 }
 
 export {countValidPassports};
