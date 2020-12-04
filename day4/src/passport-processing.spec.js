8d76db3612039bb7b29912f39e2501120aae68c2
Luis Rovirosa Mairlot <luis@codium.team>
2020-12-04 20:43:46 +0100

    [day 4] [part 2] valid passport with pid with initial 0

--- day4/src/passport-processing.spec.js
+++ day4/src/passport-processing.spec.js
@@ -2,7 +2,7 @@ import {countValidPassports} from './passport-processing';
 
 describe('valid passports', () => {
   test('sample valid passport', () => {
-    let passport = 'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\n' +
+    let passport = 'ecl:gry pid:060033327 eyr:2020 hcl:#fffffd\n' +
         'byr:1937 iyr:2017 cid:147 hgt:183cm';
     expect(countValidPassports(passport)).toBe(1);
   });
