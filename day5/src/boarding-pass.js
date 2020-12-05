ebf739f69a625780c5ad773febbe8279e9500514
Luis Rovirosa Mairlot <luis@codium.team>
2020-12-05 11:47:02 +0100

    [day 5] [part 1] refactor rename and reorder<

--- day5/src/boarding-pass.js
+++ day5/src/boarding-pass.js
@@ -2,25 +2,25 @@ function id(boardingPass) {
   return row(boardingPass)* 8 + column(boardingPass);
 }
 
+function row(boardingPass) {
+  let rows = boardingPass.slice(0, 7).split('');
+  return rows.reduce(partitionBy('F'), [0, 127])[0];
+}
+
+function column(boardingPass) {
+  const columns = boardingPass.slice(7).split('');
+  return columns.reduce(partitionBy('L'), [0, 7])[0];
+}
+
+function partitionBy(firstPartLetter) {
+  return (accumulator, position) => binaryPartition(firstPartLetter, accumulator, position);
+}
+
 function binaryPartition(firstPartLetter, accumulator, position) {
   const halfDistance = (accumulator[1] - accumulator[0] + 1) / 2;
   return position === firstPartLetter
       ? [accumulator[0], accumulator[1] - halfDistance]
       : [accumulator[0] + halfDistance, accumulator[1]];
-}
-
-function extracted(firstPartLetter) {
-  return (accumulator, position) => binaryPartition(firstPartLetter, accumulator, position);
-}
-
-function row(boardingPass) {
-  let rows = boardingPass.slice(0, 7).split('');
-  return rows.reduce(extracted('F'), [0, 127])[0];
-}
-
-function column(boardingPass) {
-  const columns = boardingPass.slice(7).split('');
-  return columns.reduce(extracted('L'), [0, 7])[0];
 }
 
 export {row, column, id};
