39f016ae6012a705bf96c91c6f6b5125e5576c41
Luis Rovirosa Mairlot <luis@codium.team>
2020-12-05 11:44:32 +0100

    [day 5] [part 1] column FBFBBFFRLR is 5

--- day5/src/boarding-pass.js
+++ day5/src/boarding-pass.js
@@ -1,12 +1,22 @@
-function binaryPartition(accumulator, position) {
+function binaryPartition(firstPartLetter, accumulator, position) {
   const halfDistance = (accumulator[1] - accumulator[0] + 1) / 2;
-  return position === 'F'
+  return position === firstPartLetter
       ? [accumulator[0], accumulator[1] - halfDistance]
       : [accumulator[0] + halfDistance, accumulator[1]];
 }
 
-function row(boardingPass) {
-  return boardingPass.slice(0, 7).split('').reduce(binaryPartition, [0, 127])[0];
+function extracted(firstPartLetter) {
+  return (accumulator, position) => binaryPartition(firstPartLetter, accumulator, position);
 }
 
-export {row};
+function row(boardingPass) {
+  let rows = boardingPass.slice(0, 7).split('');
+  return rows.reduce(extracted('F'), [0, 127])[0];
+}
+
+function column(boardingPass) {
+  const columns = boardingPass.slice(7).split('');
+  return columns.reduce(extracted('L'), [0, 7])[0];
+}
+
+export {row, column};
--- day5/src/boarding-pass.spec.js
+++ day5/src/boarding-pass.spec.js
@@ -1,8 +1,11 @@
-import {row} from './boarding-pass';
+import {row, column} from './boarding-pass';
 
 describe('Test first', () => {
     test('row FBFBBFFRLR is 44', () => {
         expect(row('FBFBBFFRLR')).toBe(44);
+    });
+    test('column FBFBBFFRLR is 5', () => {
+        expect(column('FBFBBFFRLR')).toBe(5);
     });
 
 });
