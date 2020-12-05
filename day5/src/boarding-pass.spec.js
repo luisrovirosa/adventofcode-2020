aab3f100db065536b51882a07039b94307d1ba86
Luis Rovirosa Mairlot <luis@codium.team>
2020-12-05 11:55:17 +0100

    [day 5] [part 1] ignore reformat

--- day5/src/boarding-pass.spec.js
+++ day5/src/boarding-pass.spec.js
@@ -1,24 +1,24 @@
 import {row, column, id, highest} from './boarding-pass';
 
 describe('Test first', () => {
-    test('row FBFBBFFRLR is 44', () => {
-        expect(row('FBFBBFFRLR')).toBe(44);
-    });
+  test('row FBFBBFFRLR is 44', () => {
+    expect(row('FBFBBFFRLR')).toBe(44);
+  });
 
-    test('column FBFBBFFRLR is 5', () => {
-        expect(column('FBFBBFFRLR')).toBe(5);
-    });
+  test('column FBFBBFFRLR is 5', () => {
+    expect(column('FBFBBFFRLR')).toBe(5);
+  });
 
-    test('id FBFBBFFRLR is 357', () => {
-        expect(id('FBFBBFFRLR')).toBe(357);
-    });
+  test('id FBFBBFFRLR is 357', () => {
+    expect(id('FBFBBFFRLR')).toBe(357);
+  });
 });
 
 describe('solutions', () => {
-    test('first part', () => {
-        const fs = require('fs');
-        const boardingPasses = fs.readFileSync('input.txt', 'utf8');
+  test('first part', () => {
+    const fs = require('fs');
+    const boardingPasses = fs.readFileSync('input.txt', 'utf8');
 
-        expect(highest(boardingPasses)).toBe(935);
-    });
+    expect(highest(boardingPasses)).toBe(935);
+  });
 });
