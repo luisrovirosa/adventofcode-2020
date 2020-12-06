abcdd5b33d851469a787a78262f363c17579b4d6
Luis Rovirosa Mairlot <luis@codium.team>
2020-12-06 09:48:44 +0100

    [day 6] initial commit

---
+++ day6/src/custom-customs.js
@@ -0,0 +1,5 @@
+function customCustoms() {
+  return true;
+}
+
+export {customCustoms};
---
+++ day6/src/custom-customs.spec.js
@@ -0,0 +1,8 @@
+import {customCustoms} from './custom-customs';
+
+describe('xxx', () => {
+    test('xxx', () => {
+        expect(customCustoms()).toBe(true);
+    });
+
+});
