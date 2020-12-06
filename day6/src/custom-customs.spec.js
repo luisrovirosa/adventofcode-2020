186a89343af04100552134723e913481bcf6c14a
Luis Rovirosa Mairlot <luis@codium.team>
2020-12-06 09:53:07 +0100

    [day 6] write acceptance test number of different answers

--- day6/src/custom-customs.spec.js
+++ day6/src/custom-customs.spec.js
@@ -1,8 +1,23 @@
 import {customCustoms} from './custom-customs';
 
-describe('xxx', () => {
-    test('xxx', () => {
-        expect(customCustoms()).toBe(true);
+xdescribe('acceptance test', () => {
+    test('number of different answers', () => {
+        const answers = 'abc\n' +
+            '\n' +
+            'a\n' +
+            'b\n' +
+            'c\n' +
+            '\n' +
+            'ab\n' +
+            'ac\n' +
+            '\n' +
+            'a\n' +
+            'a\n' +
+            'a\n' +
+            'a\n' +
+            '\n' +
+            'b';
+        expect(customCustoms(answers)).toBe(11);
     });
 
 });
