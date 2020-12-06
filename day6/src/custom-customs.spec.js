1d41b5852d125e9e40d59739b294b5d879c5db9c
Luis Rovirosa Mairlot <luis@codium.team>
2020-12-06 10:04:27 +0100

    [day 6] answer first part

--- day6/src/custom-customs.spec.js
+++ day6/src/custom-customs.spec.js
@@ -1,37 +1,46 @@
 import {customCustoms, differentAnswersInAGroup} from './custom-customs';
 
 describe('single groups', () => {
-   test('count the answers of 1 person group', () => {
-       expect(differentAnswersInAGroup('abc')).toBe(3);
-    });
+  test('count the answers of 1 person group', () => {
+    expect(differentAnswersInAGroup('abc')).toBe(3);
+  });
 
-   test('count the answers of multiple person group', () => {
-       expect(differentAnswersInAGroup('abc\nde\nf')).toBe(6);
-    });
+  test('count the answers of multiple person group', () => {
+    expect(differentAnswersInAGroup('abc\nde\nf')).toBe(6);
+  });
 
-   test('repeated answers does not count twice', () => {
-       expect(differentAnswersInAGroup('abc\nbc')).toBe(3);
-    });
+  test('repeated answers does not count twice', () => {
+    expect(differentAnswersInAGroup('abc\nbc')).toBe(3);
+  });
 });
 
 describe('acceptance test', () => {
-    test('number of different answers', () => {
-        const answers = 'abc\n' +
-            '\n' +
-            'a\n' +
-            'b\n' +
-            'c\n' +
-            '\n' +
-            'ab\n' +
-            'ac\n' +
-            '\n' +
-            'a\n' +
-            'a\n' +
-            'a\n' +
-            'a\n' +
-            '\n' +
-            'b';
-        expect(customCustoms(answers)).toBe(11);
-    });
+  test('number of different answers', () => {
+    const answers = 'abc\n' +
+        '\n' +
+        'a\n' +
+        'b\n' +
+        'c\n' +
+        '\n' +
+        'ab\n' +
+        'ac\n' +
+        '\n' +
+        'a\n' +
+        'a\n' +
+        'a\n' +
+        'a\n' +
+        '\n' +
+        'b';
+    expect(customCustoms(answers)).toBe(11);
+  });
 
+});
+
+describe('answers', () => {
+  test('first part', () => {
+    const fs = require('fs');
+    const answers = fs.readFileSync('input.txt', 'utf8');
+
+    expect(customCustoms(answers)).toBe(7027);
+  });
 });
